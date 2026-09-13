2b44f94521241a957fafa1f7716eef1c26e5a579
Chakhmmani Ahmed <chakhmmani999@gmail.com>
2026-09-13 17:03:20 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 1) return 0;
+        int maxDiff = 0;
+        int left = 0;
+        int right = 1;
+        int minElem = Math.min(prices[left], prices[right]);
+        // for(int i = 0; i < prices.length - 1; i++){
+        //     for(int j = i+1; j < prices.length; j++){
+        //         maxDiff = Math.max(maxDiff, prices[j] - prices[i]);
+        //     }
+        // }
+        while(right < prices.length){
+            minElem = Math.min(minElem, prices[right]);
+            maxDiff = Math.max(maxDiff, prices[right] - minElem);
+            left++;
+            right++;
+        }
+        return maxDiff;
+    }
+}
