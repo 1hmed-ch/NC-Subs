18e4c945f1fb80cda19ed9e212d3a22bb6d6e922
Chakhmmani Ahmed <chakhmmani999@gmail.com>
2026-09-26 17:55:56 +0100

    Add: valid-palindrome-ii - submission-13

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-13.java
@@ -0,0 +1,36 @@
+class Solution {
+    public static boolean isPalindrome(String s){
+        char[] charArray = s.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+        while(left < right){
+            if(charArray[left] == charArray[right]){
+                left++;
+                right--;
+            } else return false;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        char[] charArray = s.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+        int tolerance = 0;
+
+        while(left < right){
+            if(charArray[left] == charArray[right]){
+                left++;
+                right--;
+            } else {
+                tolerance++;
+                if(isPalindrome(s.substring(left+1, right + 1)) || isPalindrome(s.substring(left, right))){
+                    return true;
+                }
+                if(tolerance > 1) break;
+            }
+        }
+
+        return tolerance > 1 ? false : true;
+    }
+}
